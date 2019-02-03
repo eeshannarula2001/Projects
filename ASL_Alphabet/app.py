b95b227ed0766bd4427c3db1b8347c6f2352bf29
eeshannarula2001 <46795683+eeshannarula2001@users.noreply.github.com>
2019-02-03 23:03:17 +0530

    Add files via upload

---
+++ ASL_Alphabet/app.py
@@ -0,0 +1,30 @@
+import cv2
+import tools
+import consts
+import numpy as np
+from keras.models import load_model
+
+model = load_model('ASL96.h5')
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    cv2.rectangle(frame,(100,100),(400,400),(255,0,0),3)
+    image = frame[100:400,100:400]
+    image = cv2.resize(image,consts.shape)
+    input = np.reshape(image,consts.shape_for_nsamples(1))/255.0
+    prediction = tools.getPrediction(model.predict(input)[0])
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(frame,prediction,(600,100),font, 4,(0,0,255),2,cv2.LINE_AA)
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
