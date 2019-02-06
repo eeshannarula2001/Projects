92449d0afdb40e0bf51b04dc5304ee6299337e1b
eeshannarula2001 <46795683+eeshannarula2001@users.noreply.github.com>
2019-02-06 19:36:11 +0530

    Update app.py

--- ASL_Alphabet/app.py
+++ ASL_Alphabet/app.py
@@ -8,7 +8,13 @@ model = load_model('ASL96.h5')
 
 cap = cv2.VideoCapture(0)
 
+past = [None] * 10
+sentence = []
+
 while(True):
+
+    if len(past) > 20:
+        del past[0]
     # Capture frame-by-frame
     ret, frame = cap.read()
 
@@ -17,9 +23,21 @@ while(True):
     image = cv2.resize(image,consts.shape)
     input = np.reshape(image,consts.shape_for_nsamples(1))/255.0
     prediction = tools.getPrediction(model.predict(input)[0])
+    past.append(prediction)
+
+    if tools.check(past):
+       if not prediction == None  and not prediction == 'nothing':
+          if prediction == 'space':
+            sentence.append(' ')
+          elif prediction == 'del':
+            del sentence[len(sentence) - 1]
+          else:
+            sentence.append(prediction)
+          past = [None] * 20
 
     font = cv2.FONT_HERSHEY_SIMPLEX
     cv2.putText(frame,prediction,(600,100),font, 4,(0,0,255),2,cv2.LINE_AA)
+    cv2.putText(frame,''.join(sentence),(100,600),font, 4,(0,0,255),2,cv2.LINE_AA)
     # Display the resulting frame
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
